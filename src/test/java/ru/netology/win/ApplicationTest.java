d12357b08c8a72a2f1a540cbb47bd1bca40333b4
Maryust <okmijn0001@ya.ru>
2022-01-24 18:06:14 +0300

    Webmanager added

--- src/test/java/ru/netology/win/ApplicationTest.java
+++ src/test/java/ru/netology/win/ApplicationTest.java
@@ -1,5 +1,6 @@
 package ru.netology.win;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,7 +21,7 @@ public class ApplicationTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
